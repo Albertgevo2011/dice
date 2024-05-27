2e433200184c98c9cd792d6e89f0c297414701e5
Albertgevo2011 <gevorgjanalbert@gmail.com>
2024-05-28 02:20:15 +0400

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    game.setLife(3)
+    music.play(music.stringPlayable("F - F - F - B - ", 120), music.PlaybackMode.UntilDone)
+    if (true) {
+    	
+    }
+})
